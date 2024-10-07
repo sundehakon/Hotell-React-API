9e9d5ba1822c22474d4c22f849208f173da3c3e8
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-10-07 14:25:24 +0200

    Add DELETE

--- server.js
+++ server.js
@@ -67,6 +67,23 @@ app.post('/Blogs', authMiddleware, async (req, res) => {
     }
 });
 
+app.delete('/Blogs/:id', authMiddleware, async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const deletedPost = await Post.findByIdAndDelete(id);
+
+        if (!deletedPost) {
+            return res.status(404).json({ message: 'Blog post not found' });
+        }
+
+        res.status(200).json({ message: 'Blog post deleted successfully' });
+    } catch (error) {
+        console.error('Error deleting post:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
 app.get('/Comments', async (req, res) => {
     try {
         const comments = await Comment.find();
