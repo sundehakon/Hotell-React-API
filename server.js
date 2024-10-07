c9f997ade4de934525a047b09393820853904ffd
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-10-07 13:47:17 +0200

    Update auth code

--- server.js
+++ server.js
@@ -35,8 +35,13 @@ const Comment = mongoose.model('Comment', commentSchema);
 
 const authenticateToken = (req, res, next) => {
     const token = req.headers['authorization']?.split(' ')[1];
-    if (!token) return res.sendStatus(401);
-    next();
+    if (!token) return res.sendStatus(401); 
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403); 
+        req.user = user; 
+        next(); 
+    });
 };
 
 app.get('/Blogs', async (req, res) => {
